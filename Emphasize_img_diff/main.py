98dc0bd5b1c03d0c3972b779a9206c05f3cb5412
ishino <nonchan95425@gmail.com>
2019-06-15 21:18:45 +0900

    temp

--- Emphasize_img_diff/main.py
+++ Emphasize_img_diff/main.py
@@ -1,5 +1,18 @@
 import sys
+import numpy as np
+import cv2
 
-args = sys.argv
-for i in args:
-    print(i)
+
+def main():
+
+    img_src1 = cv2.imread("img_test/01.jpg", 1)
+    img_src2 = cv2.imread("img_test/01.jpg", 1)
+
+    fgbg = cv2.bgsegm.createBackgroundSubstractorMOG()
+
+    fgmask = fgbg.apply(img_src1)
+    fgmask = fgbg.apply(img_src2)
+
+
+if __name__ == "__main__":
+    main()
