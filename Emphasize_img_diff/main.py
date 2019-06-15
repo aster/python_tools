b584a03f7deec04b6f018ad8505f48498d48026e
ishino <nonchan95425@gmail.com>
2019-06-15 21:59:46 +0900

    first version

--- Emphasize_img_diff/main.py
+++ Emphasize_img_diff/main.py
@@ -6,12 +6,20 @@ import cv2
 def main():
 
     img_src1 = cv2.imread("img_test/01.jpg", 1)
-    img_src2 = cv2.imread("img_test/01.jpg", 1)
+    img_src2 = cv2.imread("img_test/02.jpg", 1)
 
-    fgbg = cv2.bgsegm.createBackgroundSubstractorMOG()
+    fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 
     fgmask = fgbg.apply(img_src1)
     fgmask = fgbg.apply(img_src2)
+
+    cv2.imshow('frame', fgmask)
+
+    bg_diff_path = './diff.jpg'
+    cv2.imwrite(bg_diff_path, fgmask)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
