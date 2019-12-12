4c4a25f00b6832dc2731bcef7abec5fa78aff122
ishino <nonchan95425@gmail.com>
2019-12-12 15:36:37 +0900

    hoge

--- serial2csv/serial2csv.py
+++ serial2csv/serial2csv.py
@@ -2,13 +2,23 @@ import serial
 import csv
 import codecs
 import sys
+import os
 
 # 接続されてるCOMポート
 com_port = 'COM3'
 #保存したいファイル名
-file_name = 'data.csv'
 
-com = serial.Serial(com_port)
+# com = serial.Serial(com_port)
+current_dir = os.getcwd().split('\\')[-1]
+exist_files = os.listdir()
+print(exist_files)
+file_name = "{}".format(current_dir) +'_01'
+print(file_name)
+
+print('\n"{}"のデータを"{}"に記録します。'.format(com_port,file_name))
+print('データの取得を終了するときは"Ctrl+C"\n')
+
+sys.exit(0)
 
 try:
     with open(file_name, 'w', newline='') as f:
