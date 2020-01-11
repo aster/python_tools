06f8489bb866e663389dc3a6d7f95681b6ebc46a
ishino <nonchan95425@gmail.com>
2020-01-11 19:16:04 +0900

    2020年 1月 11日 土曜日 19:16:04

--- ucchi_marge/trim.py
+++ ucchi_marge/trim.py
@@ -5,10 +5,16 @@ args = sys.argv
 
 for movie_index in range(1,6):
     for position_index in range(1,4):
-#
-#def main():
-#    with open(file) as f:
-#        reader = csv.reader(f)
         print(str(movie_index) + " "+ str(position_index) )
-
+        trimData()
+    joinTrimmedData()
         
+
+#データを必要な部分だけに加工する
+def trimData():
+    print()
+         
+#同じ動画のフォルダ内にあるデータを結合する
+def joinTrimmedData():
+    print()
+    
