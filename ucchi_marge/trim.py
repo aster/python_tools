ab2f8a6ca5b41d49cf3a6c9f5089207dfa3c3fd7
ishino <nonchan95425@gmail.com>
2020-01-11 20:40:20 +0900

    2020年 1月 11日 土曜日 20:40:19

--- ucchi_marge/trim.py
+++ ucchi_marge/trim.py
@@ -6,8 +6,6 @@ from pathlib import Path
 args = sys.argv
 
 # 結合するデータ一覧を取得する
-
-
 def getJoinDataList(movie_index, position_index):
     file_path = './0%d/0%d/' % (movie_index, position_index)
     # csv_listに、例：動画1のベテラン20人分 のデータのPathが入ってる
@@ -15,11 +13,8 @@ def getJoinDataList(movie_index, position_index):
     return csv_list
 
 # 同じ動画番号フォルダ内にあるデータを結合する
-
-
 def joinData(movie_index):
     print()
-
 
 # 動画の種類（1~5）でループ  一時的に6から2に
 for movie_index in range(1, 2):
@@ -28,7 +23,6 @@ for movie_index in range(1, 2):
         join_all_list = getJoinDataList(movie_index, position_index)
 
         for personal_data in join_all_list:
-            # print(personal_data)
             # 個人のデータを加工して、出力用データに追加する
             # 0,1行目捨て,2行目から数字のインデックスの間を取得
             # 0列目捨て、1列目から6列分取得
@@ -36,7 +30,14 @@ for movie_index in range(1, 2):
                 break
             with open(personal_data) as f:
                 reader = csv.reader(f)
+                first_2row = 0 
+
                 for row in reader:
-                    if not row[1].isdigit():
+                    #最初の2行と、最後の数字以降の行を排除
+                    if first_2row < 2:
+                        first_2row += 1
+                        continue
+                    elif not row[1].isdigit() :
                         break
+
                     print(row[1:7])
