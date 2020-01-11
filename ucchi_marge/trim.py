870b4ece9bf4137052c4affe96008fb8b7887b26
ishino <nonchan95425@gmail.com>
2020-01-11 19:10:13 +0900

    2020年 1月 11日 土曜日 19:10:13

--- ucchi_marge/trim.py
+++ ucchi_marge/trim.py
@@ -4,7 +4,7 @@ import sys
 args = sys.argv
 
 for movie_index in range(1,6):
-    for position_index in rnage(1,4):
+    for position_index in range(1,4):
 #
 #def main():
 #    with open(file) as f:
