e23009e2d44b827e0aad62729112920d07325d72
ishino <nonchan95425@gmail.com>
2020-01-11 19:36:50 +0900

    2020年 1月 11日 土曜日 19:36:50

--- ucchi_marge/trim.py
+++ ucchi_marge/trim.py
@@ -4,6 +4,17 @@ from pathlib import Path
 
 args = sys.argv
 
+#データを必要な部分だけに加工する
+def trimData(movie_index, position_index):
+    file_path = './0%d/0%d' % (movie_index, position_index)
+    print(file_path)
+         
+#同じ動画のフォルダ内にあるデータを結合する
+def joinTrimmedData(movie_index):
+    print()
+    
+
+
 for movie_index in range(1,6):
     for position_index in range(1,4):
         #print(str(movie_index) + " "+ str(position_index) )
@@ -11,15 +22,3 @@ for movie_index in range(1,6):
     #joinTrimmedData(movie_index)
         
 
-
-#データを必要な部分だけに加工する
-def trimData(movie_index, position_index):
-    file_path = './0%d/0%d' % (movie_index, position_index)
-    print(file_path)
-
-    
-         
-#同じ動画のフォルダ内にあるデータを結合する
-def joinTrimmedData(movie_index):
-    print()
-    
